06b0d48c53f3ea05fee8aed9d7d4ff8dd29f3bc8
Evgeny Kankin <evgenykankin@gmail.com>
2026-07-01 19:48:19 +0300

    strict mode added

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import { MantineProvider, createTheme } from '@mantine/core'
 import { Provider } from 'react-redux';
 import { store } from './features/store';
 import App from './App.tsx';
-import '@fontsource/open-sans/500.css'; // Средний вес (500)
-import '@fontsource/open-sans/700.css'; // Жирный (700)
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/700.css';
+import { StrictMode } from 'react'
 
 const theme = createTheme({
   primaryColor: 'indigo',
 });
 
 createRoot(document.getElementById('root')!).render(
+<StrictMode> 
   <Provider store={store}>
     <MantineProvider theme={theme}>
         <App />
     </MantineProvider>
   </Provider>
+</StrictMode> 
 )
