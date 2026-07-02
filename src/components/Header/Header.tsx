5c8f954a01e101ff51696185ea8b60ceb53700bb
Evgeny Kankin <evgenykankin@gmail.com>
2026-07-02 12:52:29 +0300

    fine tuning

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import classes from './Header.module.css'
 
-function Header () {
+function Header() {
+  const navigate = useNavigate();
 
-    return (
-        <div className={classes.header}>
-            <div className={classes.logo}></div>
-            <button className={classes.vacanciesButton}></button>
-            <button className={classes.aboutButton}></button>
-        </div>
-    );
+  return (
+    <div className={classes.header}>
+      <div className={classes.logo}></div>
+
+      <button
+        className={classes.vacanciesButton}
+        onClick={() => navigate('/vacancies')}
+      />
+
+      <button className={classes.aboutButton}></button>
+    </div>
+  );
 }
 
 export default Header;
