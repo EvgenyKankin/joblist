bd7cc027ff90753e3561c19f2f58b56c98cd9f15
Evgeny Kankin <evgenykankin@gmail.com>
2026-07-03 20:04:40 +0300

    fine tuning

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,8 +1,26 @@
-export default function NotFoundPage() {
+import classes from './NotFoundPage.module.css'
+import { useNavigate } from 'react-router-dom';
+
+function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>404</h1>
-      <p>Страница не найдена</p>
+    <div className={classes.notfoundContainer}>
+      <div className={classes.notfoundWrapper}>
+        <div className={classes.title}>
+          <h1 className={classes.notfoundTitle}>Упс! Такой страницы не существует</h1>
+          <p className={classes.notfoundText}>Давайте перейдем к началу.</p>
+        </div>
+        <button
+          className={classes.homeButton}
+          onClick={() => navigate('/vacancies/moscow')}
+        ></button>
+      </div>
+      <div className={classes.imgContainer}>
+        <img className={classes.catImg} alt='cat' src='src\assets\cat.png' />
+      </div>
     </div>
   );
 }
+
+export default NotFoundPage;
